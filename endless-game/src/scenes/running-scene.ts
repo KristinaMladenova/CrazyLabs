cedaff7d142221f4d8ff983a80d85c9d9cdc0331
KristinaMladenova <gglinv@proton.me>
2023-04-01 22:23:50 +0200

    fixed generating balls and player position

--- endless-game/src/scenes/running-scene.ts
+++ endless-game/src/scenes/running-scene.ts
@@ -1,16 +1,14 @@
 import * as THREE from "three";
-import TWEEN, { Tween } from "@tweenjs/tween.js";
+import TWEEN, { update } from "@tweenjs/tween.js";
+import { Box3, Vector3 } from "three";
 
 export default class RunningScene extends THREE.Scene {
   private geometry = new THREE.PlaneGeometry(2, 100);
   private material = new THREE.MeshPhongMaterial({ color: 0x616497 });
-
   public fogColor = 0xb4d3ef;
   public density = 0.2;
-
-  startTime = 0;
-  GAME_DURATION = 10;
-
+  private startTime = 0;
+  private GAME_DURATION = 10;
   private plane = new THREE.Mesh(this.geometry, this.material);
 
   planePosition() {
@@ -19,104 +17,40 @@ export default class RunningScene extends THREE.Scene {
   }
 
   private clock = new THREE.Clock();
-
   private playerSize = new THREE.CylinderGeometry(0.3, 0.3, 0.3, 3);
   private playerMaterial = new THREE.MeshPhongMaterial({ color: 0x4d3d64 });
   private player = new THREE.Mesh(this.playerSize, this.playerMaterial);
-
-  private moveLeft() {
-    if (this.player.position.x !== -18) {
-      const tweenLeft = new TWEEN.Tween(this.player.position)
-        .to({ x: this.player.position.x - 18 }, 200)
-        .easing(TWEEN.Easing.Quadratic.Out)
-        .onUpdate(() => {
-          this.player.rotation.y = -140 * (Math.PI / 180);
-          if (this.player.position.x < -18) {
-            this.player.position.x = -18;
-          }
-        })
-        .onComplete(() => {
-          this.player.rotation.y = 180 * (Math.PI / 180);
-        });
-      tweenLeft.start();
-    }
-  }
-  private moveRight() {
-    if (this.player.position.x !== 18) {
-      const tweenRight = new Tween(this.player.position)
-        .to({ x: this.player.position.x + 18 }, 200)
-        .easing(TWEEN.Easing.Quadratic.Out)
-        .onUpdate(() => {
-          this.player.rotation.y = 140 * (Math.PI / 180);
-
-          if (this.player.position.x >= 18) {
-            this.player.position.x = 18;
-          }
-        })
-        .onComplete(() => {
-          this.player.rotation.y = 180 * (Math.PI / 180);
-        });
-      tweenRight.start();
-    }
-  }
-
   private spheres: THREE.Mesh[] = [];
-  private sphereCount = Math.floor(Math.random() * 100) + 1;
+  private sphereCount = Math.floor(Math.random() * 200) + 50;
+  private sphereMaterial = new THREE.MeshPhongMaterial({ color: 0xe7f5f6 });
 
   private generateSpheres() {
     for (let i = 0; i < this.sphereCount; i++) {
       const sphereGeometry = new THREE.SphereGeometry(0.1, 16, 16);
-
-      const sphere = new THREE.Mesh(sphereGeometry, this.material);
+      const sphere = new THREE.Mesh(sphereGeometry, this.sphereMaterial);
       sphere.receiveShadow = true;
       sphere.castShadow = true;
-      sphere.position.x = Math.floor(Math.random() * 2);
-      sphere.position.y = Math.floor(Math.random() * 100);
-      sphere.position.z = -Math.floor(Math.random() * 2);
-      this.player.rotation.x = -90;
 
+      sphere.position.x = Math.floor(Math.random() * 2) - 0.5;
+      sphere.position.y = Math.floor(Math.random() * 100) - 0.2;
+      sphere.position.z = -0.96;
       this.add(sphere);
+      const axes = new THREE.AxesHelper(1);
+      sphere.add(axes);
       this.spheres.push(sphere);
-    }
-  }
-
-  private updateSpheres(deltaTime: number) {
-    for (let i = 0; i < this.spheres.length; i++) {
-      const sphere = this.spheres[i];
-      const speed = 1; // Change this to adjust the speed of the spheres
-      sphere.position.z += speed * deltaTime;
-
-      // Check for collision with the player
-      const distance = this.player.position.distanceTo(sphere.position);
-      if (distance < 1) {
-        // Particle effect
-        const particle = this.createParticleEffect(sphere.position);
-        this.add(particle);
-
-        // Remove the sphere from the scene and array
-        this.remove(sphere);
-        this.spheres.splice(i, 1);
-
-        // Add to the score
-        this.score++;
-        this.updateScoreUI();
-
-        // Display the button if the player reaches a certain score
-        if (this.score >= 10) {
-          this.displayButton();
-        }
-      }
     }
   }
 
   async load() {
     this.fog = new THREE.FogExp2(this.fogColor, this.density);
     this.plane.receiveShadow = true;
+
     this.plane.position.y = 0;
     this.plane.position.z = -1;
 
     this.player.position.y = 1.2;
-    this.player.position.z = -0.9;
+    this.player.position.z = -0.7;
+
     this.player.rotation.x = -90;
     this.player.rotation.y = 0;
     this.player.rotation.z = 0;
@@ -156,24 +90,18 @@ export default class RunningScene extends THREE.Scene {
   }
 
   initialize() {
-    // ...
     const canvas = document.querySelector("canvas");
     canvas!.addEventListener("mousemove", this.onMouseMove.bind(this));
+    this.generateSpheres();
 
     const axesHelper = new THREE.AxesHelper(1);
     this.player.add(axesHelper);
-    this.generateSpheres();
   }
+
   private onMouseMove(event: MouseEvent) {
     const canvas = document.querySelector("canvas");
-    const canvasBounds = canvas!.getBoundingClientRect();
-    const canvasCenterX = canvasBounds.left + canvasBounds.width / 1;
-    const mousePositionX = event.clientX;
-
-    const distanceFromCenter = mousePositionX - canvasCenterX;
-    const playerVelocity = (distanceFromCenter / canvasBounds.width) * 1;
-
-    this.player.position.x += playerVelocity;
+    const x = event.clientX / canvas!.clientWidth;
+    this.player.position.x = (x * 2 - 1) * 2;
   }
 
   update() {
@@ -181,13 +109,15 @@ export default class RunningScene extends THREE.Scene {
     if (this.startTime !== null && elapsedTime >= this.GAME_DURATION) {
       // End the game
       console.log("Game over!");
-      this.startTime = null;
+      // this.startTime = null;
       const div = document.getElementById("game-over-modal");
-      div.style.display = "flex";
+      // div.style.display = "flex";
 
-      this.updateSpheres();
+      // this.updateSpheres();
 
       TWEEN.update();
+
+      this.detectCollisionWithCoins();
     }
   }
 
